60acf451373fd3e50726ed869b99be0dce7bfb4e
Wendell Adriel <wendelladriel.ti@gmail.com>
2019-01-08 12:33:34 +0000

    added custom html template and added introduction text

---
+++ src/html.js
@@ -0,0 +1,44 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default class HTML extends React.Component {
+  render() {
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+          <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us7.list-manage.com","uuid":"cfe9b6c389d6beecd5ac3dc79","lid":"a6efef1c06","uniqueMethods":true}) })`,
+            }}
+          />
+        </body>
+      </html>
+    )
+  }
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -27,16 +27,23 @@ const keywords = [
   'node',
 ]
 
-const ComingSoon = styled.div`
+const Introduction = styled.div`
   width: 100%;
   text-align: center;
-  font-family: ${theme.fonts.cursive};
-  font-size: 5rem;
-  font-weight: 700;
+  font-size: 2rem;
+  margin-bottom: 2rem;
 
   ${breakpoints.md} {
-    font-size: 7rem;
+    font-size: 3rem;
   }
+
+  a {
+    border-bottom: 1px dashed ${theme.colors.primary};
+  }
+`
+
+const Highlight = styled.span`
+  font-weight: 700;
 `
 
 const IndexPage = () => (
@@ -46,7 +53,24 @@ const IndexPage = () => (
       keywords={keywords}
       description={`Bugs n' Coffees is my personal blog that I'll write about software development (mostly related to JS) where the articles will mix storytelling with references to music lyrics while exploring topics and tutorials about software development.`}
     />
-    <ComingSoon>Coming soon...</ComingSoon>
+    <Introduction>
+      <Highlight>Hey there, I'm <a href="https://wendelladriel.com" target="_blank" rel="noopener noreferrer">Wendell Adriel</a> 👋</Highlight><br />
+      I'm a Software Engineer and I've been working with software development since 2009. Besides that, I'm a <Highlight>BIG FAN</Highlight> of tabletop RPG games.
+      I play D&D since 2008 as a Dungeon Master and I really love fantasy worlds, that's one of the reasons I love to be a Dungeon Master because
+      I can create these worlds.
+    </Introduction>
+
+    <Introduction>
+      <Highlight>Bugs n' Coffees (B&C)</Highlight> is my new personal project, where I'll merge things I love: Software Development, Fantasy Worlds, Storytelling and Music.
+      You must be thinking, how do you plan to do that? 🤔🤔🤔
+    </Introduction>
+
+    <Introduction>
+      This will be a different blog, where <Highlight>the articles will be written as a history</Highlight> and the main character will need to solve bugs, challenges
+      and puzzles with code to advance in the journey. That way, you will not only learn topics about Software Development, but also will follow an interesting journey
+      of <Highlight>Eva, a 18 years old girl in the beginning of her career into Software Development, into IOLand, a Fantasy World
+      ruled by code.</Highlight>
+    </Introduction>
   </Layout>
 )
 
